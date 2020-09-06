3b92bdd52b64ffc96308e2987f691d9f17a41bed
Wayne Duran <asartalo@gmail.com>
2020-09-07 03:06:39 +0800

    CI: Testing php+apache workflow 2

--- .github/actions/php-apache/entrypoint.sh
+++ .github/actions/php-apache/entrypoint.sh
@@ -1,15 +1,14 @@
 #!/bin/sh -l
 
-echo "Hello from inception server :)"
+echo "Port is $1"
 pwd
+
+ls -la /
+cp /entrypoint-server.sh ./entrypoint.sh
+cp /Dockerfile ./Dockerfile
+echo "After copy..."
 ls -la
 
-# sh -c "$docker_run"
-
-# cat /etc/apache2/sites-available/000-default.conf
-mkdir -p /var/www/html/uploads
-chown -R www-data:www-data /var/www && a2enmod rewrite
-service apache2 restart
-ls -la /var/log/apache2
-ls -la /var/www/html
-cat /var/www/html/index.php
+echo "Docker inception start..."
+docker build . -t lms
+docker run -d -p $1:80 --name docker-lms lms
