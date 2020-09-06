415865b0a6cdf133f54cfb419a783244dd14ecb3
Wayne Duran <asartalo@gmail.com>
2020-09-07 03:26:51 +0800

    CI: Testing php+apache workflow 5

--- .github/actions/php-apache/entrypoint.sh
+++ .github/actions/php-apache/entrypoint.sh
@@ -1,16 +1,12 @@
 #!/bin/sh -l
 
-echo "Port is $1"
-pwd
-
 ls -la /
 cp /entrypoint-server.sh ./entrypoint-server.sh
 cp /Dockerfile ./Dockerfile
 echo "After copy..."
 ls -la
-cat Dockerfile
 
-echo "Docker inception start..."
+echo "Exposing docker on port $1..."
 docker build . -t lms
-# docker run -d -p $1:80 --name docker-lms lms
-docker run -p $1:80 --name docker-lms lms
+docker run -d -p $1:80 --name docker-lms lms
+# docker run -p $1:80 --name docker-lms lms
